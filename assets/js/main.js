b6e2a9d19590c8b29d89a8e805ed53b46128897a
tpapillionjr <tpapillionjr@gmail.com>
2026-05-21 23:31:21 -0500

    added analytics for vercel

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,7 @@
+    import { inject } from "@vercel/analytics";
+
+    inject();
+
     const pageScroll = document.getElementById("page-scroll");
     const pageScrollbar = document.getElementById("page-scrollbar");
     const pageScrollbarThumb = document.getElementById("page-scrollbar-thumb");
